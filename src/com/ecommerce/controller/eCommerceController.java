38e64dfaad224788cb8fa7f8a066ef7823b58c74
Noel <noel.cheng@rutgers.edu>
2020-10-01 23:31:04 -0400

    implement purchase function

--- src/com/ecommerce/controller/eCommerceController.java
+++ src/com/ecommerce/controller/eCommerceController.java
@@ -2,13 +2,22 @@ package com.ecommerce.controller;
 
 import com.ecommerce.data.InitialData;
 import com.ecommerce.model.Customer;
+import com.ecommerce.model.Invoice;
 import com.ecommerce.model.Product;
 import com.ecommerce.service.eCommerceService;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.time.format.DateTimeFormatter;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class eCommerceController {
+
+    public static NumberFormat formatter = new DecimalFormat("00.00");
+    public static String formatPrice;
+    public static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("h:s a  MM/dd/yyyy");
 
     public static void mainMenu(){
         System.out.println("Welcome!");
@@ -116,6 +125,8 @@ public class eCommerceController {
 
                 if(password.equals(eCommerceService.loginCheck().get(user_id))){
                     System.out.println("Successfully logged in!");
+                    loggedMenu(eCommerceService.getCustomerByUsername(user_id));
+
                 }else{
                     System.out.println("Username and Password do not match");
                 }
@@ -123,6 +134,99 @@ public class eCommerceController {
             }else{
                 System.out.println("Username does not exist!");
             }
+    }
+
+    public static void loggedMenu(Customer customer) {
+
+        int choice = 0;
+        do {
+
+            Scanner input = new Scanner(System.in);
+
+            System.out.println();
+            System.out.println("    Item " + "     " + " Price " + "  " + "Item Code ");
+            System.out.println("-----------------------------------");
+
+            for (Product p : eCommerceService.getProducts()) {
+                formatPrice = formatter.format(p.getPrice());
+                String productLine = String.format("   %-10s  $%-5s      %-3d", p.getProductName(), formatPrice, p.getProductCode());
+                System.out.println(productLine);
+            }
+
+            System.out.println();
+            System.out.println("1. Purchase Item(s)");
+            System.out.println("2. Return item");
+            System.out.println("3. Log Out");
+
+            choice = input.nextInt();
+
+            if(choice == 1){
+                purchase(customer);
+            }else if(choice == 2){
+                returns();
+            }else if(choice < 1 || choice > 3){
+                System.out.println("Not a valid choice. Please input a valid option.");
+                choice = input.nextInt();
+            }
+
+        }while(choice != 3);
+    }
+
+    public static void purchase(Customer customer) {
+        Scanner input = new Scanner(System.in);
+        String purchaseCodes;
+        List<Product> purchaseList;
+
+        System.out.println("Enter the code(s) of what you want to purchase separated by , (i.e. 1,3,5)");
+        purchaseCodes = input.next();
+
+        purchaseList = eCommerceService.purchaseList(purchaseCodes);
+
+        for(Product p : purchaseList){
+            formatPrice = formatter.format(p.getPrice());
+            String purchase = String.format("   %-3d  %-10s  $%-5s  ", p.getProductCode(), p.getProductName(), formatPrice);
+            System.out.println(purchase);
+        }
+
+        System.out.println("Confirm purchase? (Y/N)");
+        String confirm = input.next();
+
+        if(confirm.toUpperCase().equals("Y")){
+            Invoice newInvoice = new Invoice(customer, purchaseList);
+            eCommerceService.addInvoice(newInvoice);
+
+            System.out.println();
+            System.out.println("                    Receipt");
+            System.out.println("------------------------------------------------");
+
+            Invoice invoice = eCommerceService.getInvoiceByNumber(newInvoice.getInvoiceNumber());
+            showInvoice(invoice);
+
+            purchaseList.clear();
+        }else if(confirm.toUpperCase().equals("N")){
+            purchaseList.clear();
+        }
+
+    }
+
+    public static void returns() {
+
+    }
+
+    public static void showInvoice(Invoice invoice) {
+
+        String datetime = invoice.getPurchaseDate().format(dateTimeFormatter);
+        System.out.println("Customer Name: " + invoice.getCustomerName() + "    " + "Date: " + datetime);
+        System.out.println("Invoice Number: " + invoice.getInvoiceNumber());
+        for(Product p : invoice.getPurchaseList()){
+            formatPrice = formatter.format(p.getPrice());
+            String purchase = String.format("   %-3d  %-10s  $%-5s  ", p.getProductCode(), p.getProductName(), formatPrice);
+            System.out.println(purchase);
+        }
+        System.out.println();
+        NumberFormat totalFormatter = new DecimalFormat("000.00");
+        formatPrice = totalFormatter.format(invoice.getTotal());
+        System.out.println("Total: $" + formatPrice);
     }
 
 }
--- src/com/ecommerce/service/eCommerceService.java
+++ src/com/ecommerce/service/eCommerceService.java
@@ -1,6 +1,7 @@
 package com.ecommerce.service;
 
 import com.ecommerce.model.Customer;
+import com.ecommerce.model.Invoice;
 import com.ecommerce.model.Product;
 
 import java.util.*;
@@ -10,13 +11,17 @@ public class eCommerceService {
     public static List<Customer> customerList = new ArrayList<>();
     public static List<Product> productList = new ArrayList<>();
     public static Map<String, String> customerLogin = new HashMap<>();
+    public static Map<Integer, Product> productMap = new HashMap<>();
+    public static List<Product> purchaseList = new ArrayList<>();
+    public static List<Invoice> invoiceList = new ArrayList<>();
 
     public static void addCustomer(Customer customer){
         customerList.add(customer);
     }
 
-    public static List<Customer> getCustomers() {
-        return customerList;
+    public static Customer getCustomerByUsername(String username) {
+        Customer customer = customerList.stream().filter(c -> c.getUserId().equals(username)).findAny().orElse(null);
+        return customer;
     }
 
     public static Map<String, String> loginCheck() {
@@ -33,6 +38,42 @@ public class eCommerceService {
 
     public static List<Product> getProducts() {
         return productList;
+    }
+
+    public static List<Product> purchaseList(String purchaseCodes) {
+        String[] splitCodes = purchaseCodes.split(",");
+        int[] codes = new int[splitCodes.length];
+
+        for(int i = 0; i < splitCodes.length; i++) {
+            codes[i] = Integer.parseInt(splitCodes[i]);
+        }
+
+        for(int c : codes){
+            purchaseList.add(getProductByCode(c));
+        }
+
+        return purchaseList;
+    }
+
+    public static Product getProductByCode(int code) {
+        Product product;
+
+        for(Product p : productList){
+            productMap.put(p.getProductCode(), p);
+        }
+
+        product = productMap.get(code);
+
+        return product;
+    }
+
+    public static void addInvoice(Invoice invoice) {
+        invoiceList.add(invoice);
+    }
+
+    public static Invoice getInvoiceByNumber(int number) {
+        Invoice invoice = (Invoice) invoiceList.stream().filter(i -> i.getInvoiceNumber() == number).findAny().orElse(null);
+        return invoice;
     }
 
 }
